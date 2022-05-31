d24583998844abe6e20a6e39c1bc53cdf66beb56
helenjim49 <helenjim49@gmail.com>
2022-05-31 19:26:40 +0200

    change hashDocument in hashing. revise PreviewHashing exception

--- src/de/tum/in/ase/eist/ExamSystem.java
+++ src/de/tum/in/ase/eist/ExamSystem.java
@@ -5,7 +5,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public final class ExamSystem {
-//	private static HashFunction hashFunction;
 	private ExamSystem() {
 	}
 
@@ -15,8 +14,18 @@ public final class ExamSystem {
 	}
 
 	public static void main(String[] args) throws IOException {
-		String file1 = readFile("exams/short_exam.txt");
-		String file2 = readFile("exams/long_exam.txt");  //This file is too big for Preview Hashing
+		String file1;
+		String file2;
+		try {
+			file1 = readFile("exams/short_exam.txt");
+		} catch (RuntimeException e) {
+			throw new RuntimeException("Cannot read short exam file!");
+		}
+		try {
+			file2 = readFile("exams/long_exam.txt");  //This file is too big for Preview Hashing
+		} catch (RuntimeException e) {
+			throw new RuntimeException("Cannot read short exam file!");
+		}
 
 		// TODO 6: Change SimpleHash to PreviewHashing - done
 		Hashing simpleHash = new PreviewHashing();
@@ -36,13 +45,9 @@ public final class ExamSystem {
 		System.out.println(hashFile(file2, cryptoSecureHash));
 	}
 
-	public static String readFile(String filepath) throws RuntimeException {
+	public static String readFile(String filepath) throws IOException {
 		Path path = Path.of(filepath);
 		// TODO 4: Return the content of the passed file as a String - done
-		try {
-			return Files.readString(path);
-		} catch (IOException e) {
-			throw new RuntimeException("Cannot read short exam file!");
-		}
+		return Files.readString(path);
 	}
 }
--- src/de/tum/in/ase/eist/Hashing.java
+++ src/de/tum/in/ase/eist/Hashing.java
@@ -1,7 +1,12 @@
 package de.tum.in.ase.eist;
 
 public abstract class Hashing implements HashFunction {
-    private HashFunction hashFunction;
+    private HashFunction hashFunction = new HashFunction() {
+        @Override
+        public String calculateHashCode(String input) {
+            return input;
+        }
+    };
 
     public Hashing() { }
 
